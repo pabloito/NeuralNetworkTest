2bf5918b9ed8ec7746afec548db04622b31cb334
Juan Godfrid <jgodfrid@itba.edu.ar>
2019-05-06 15:03:40 -0300

    colons

--- MultiLayerPerceptron/terrain.m
+++ MultiLayerPerceptron/terrain.m
@@ -5,13 +5,13 @@ if(N<=0 || N>14)
 endif
 
 if(N<10)
-  terrain = dlmread(strcat("terrain/terrain0",num2str(N),".data"))
+  terrain = dlmread(strcat("terrain/terrain0",num2str(N),".data"));
 else
-  terrain = dlmread(strcat("terrain/terrain",num2str(N),".data"))
+  terrain = dlmread(strcat("terrain/terrain",num2str(N),".data"));
 endif
 
-E = terrain(1:end, 1:2)
-S = terrain(1:end, 3)
+E = terrain(1:end, 1:2);
+S = terrain(1:end, 3);
   
 NN = GenericMultiLayerPerceptron();
 output = NN.train_weights(E,S);
