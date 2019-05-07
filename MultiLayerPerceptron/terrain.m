2b7656598fc2aea94181967840c0a56c3cedb53c
Juan Godfrid <jgodfrid@itba.edu.ar>
2019-05-07 17:20:40 -0300

    normalize and analyze % of sample

--- MultiLayerPerceptron/terrain.m
+++ MultiLayerPerceptron/terrain.m
@@ -1,26 +1,48 @@
 function output = terrain(N)
-if(N<=0 || N>14)
-  display("flasheaste");
-  return;
-endif
+  source("config.conf");
 
-if(N<10)
-  terrain = dlmread(strcat("terrain/terrain0",num2str(N),".data"));
-else
-  terrain = dlmread(strcat("terrain/terrain",num2str(N),".data"));
-endif
+  if(N<=0 || N>14)
+    display("flasheaste");
+    return;
+  endif
 
-E = terrain(1:end, 1:2);
-S = terrain(1:end, 3);
-  
-NN = GenericMultiLayerPerceptron();
-output = NN.train_weights(E,S);
+  if(N<10)
+    terrain = dlmread(strcat("terrain/terrain0",num2str(N),".data"));
+  else
+    terrain = dlmread(strcat("terrain/terrain",num2str(N),".data"));
+  endif
 
-# Grafico terreno correcto
-plot3(E(:,1), E(:,2), S, ".");
-title ("Terrain");
+  E = terrain(1:end, 1:2);
+  S = terrain(1:end, 3);
 
-# calculated_output = calcular S con outputs.weights para plotear
-# plot3(E(:,1), E(:,2), calculated_output);  
-title ("Neural network's interpretation");
+  E = normalize(E,-1.7,1.7);
+  S = normalize(S,-1.7,1.7);
+
+  [E,S] = remove_random(1-sample_percentage, E, S);
+    
+  NN = GenericMultiLayerPerceptron();
+  output = NN.train_weights(E,S);
+
+  # Grafico terreno correcto
+  plot3(E(:,1), E(:,2), S, ".");
+  title ("Terrain");
+
+  # calculated_output = calcular S con outputs.weights para plotear
+  # plot3(E(:,1), E(:,2), calcu lated_output);  
+  title ("Neural network's interpretation");
+
+  function norm = normalize(m, x, y)
+    norm = m - min(m(:));
+    norm = norm ./ max(norm(:));
+    norm = norm*(y-x) + x;
+  end
+
+  function [E,S] = remove_random(percentage, E, S)
+    to_remove = floor(percentage * length(E));
+    for i=1:to_remove
+      r = randi([1,length(E)]);
+      E(r,:) = [];
+      S(r) = [];
+    endfor
+  endfunction
 endfunction
