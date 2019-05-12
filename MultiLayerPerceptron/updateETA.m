d8c0310d40026fc4264d3fe24bac23b3b6666059
Constanza De Rienzo <cderienzo@itba.edu.ar>
2019-05-12 15:13:18 -0300

    changes in eta

--- MultiLayerPerceptron/updateETA.m
+++ MultiLayerPerceptron/updateETA.m
@@ -2,30 +2,32 @@ function eta_updates = updateETA(current_error, eta_plot, eta_updates)
   global NN;
   persistent previous_errors = [];
   persistent previous_errors_diff = [0];
-  persistent k = 0;
-    
-  if ( k < NN.learning_rate_k)
+  persistent k = 1;
+   
+  if(k>1)
+    if(current_error>previous_errors(k-1))
+      NN.learning_rate = NN.learning_rate * (1-NN.learning_rate_b);          
+    endif
+  endif
+  
+  if ( k <= NN.learning_rate_k)
     previous_errors = [previous_errors current_error];
-    if ( k < NN.learning_rate_k -1)
+    if ( k <= NN.learning_rate_k -1)
       previous_errors_diff = [previous_errors_diff current_error];
     endif
     k++;
   else
-    k = 0;
+    k = 1;
     sumvector = previous_errors-previous_errors_diff;
     sumvector = sign(sumvector);
     sum = sum(sumvector);
-    if(sum>0)
-      NN.learning_rate = NN.learning_rate * (1-NN.learning_rate_b);          
-     elseif(sum<0)
+    if(sum<0)
       NN.learning_rate = NN.learning_rate + NN.learning_rate_a;          
-    endif
-    
+    endif    
     #figure(eta_plot);
     #plot(eta_updates, NN.learning_rate, '.', 'markersize', 15, 'color', 'b');
     #hold on
-    eta_updates = eta_updates + 1;
-    
+    eta_updates = eta_updates + 1;   
     previous_errors = [];
     previous_errors_diff = [0];
   endif       
